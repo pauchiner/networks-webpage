adadc6736e3b7d117e2e91526141bc8e02beb0ba
pauchiner <calmgamessuport@gmail.com>
2022-12-11 16:35:12 +0100

    Add custom theme, fonts and color mode functionality. 🌚

---
+++ components/fonts.tsx
@@ -0,0 +1,9 @@
+import { Global } from "@emotion/react";
+
+const Fonts = () => (
+  <Global
+    styles={`@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500&display=swap');`}
+  />
+)
+
+export default Fonts;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,19 @@
-import { Text, Box, Container } from "@chakra-ui/react";
+import { Text, Box, Container, Heading } from "@chakra-ui/react";
+import Section from "../components/section";
+import { motion } from "framer-motion";
 
 const Page = () => {
   return (
-    <Container>
-      <Box borderRadius="lg" padding={3} background="teal" textAlign="center">
-        <Text fontSize="lg" color="white">Hello World!</Text>
+    <Container as={motion.div} initial={{ paddingTop: 10, opacity: 0 }} animate={{ paddingTop: 0, opacity: 1 }} >
+      <Box marginTop={10} textAlign="center">
+        <Text fontSize="lg" >Welcome to my webpage</Text>
       </Box>
+      <Section delay={0.1}>
+        <Heading as="h3" variant="section-title">
+          Tema 1
+        </Heading>
+        <p>Contenido</p>
+      </Section>
     </Container>
   )
 }
