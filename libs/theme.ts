b204ebebbebcad7dbb7ce632a5ae0969272d411e
pauchiner <calmgamessuport@gmail.com>
2022-12-13 16:47:26 +0100

    Change the background color of the theme. 🎨

--- libs/theme.ts
+++ libs/theme.ts
@@ -4,7 +4,7 @@ import { mode } from '@chakra-ui/theme-tools';
 const styles = {
   global: (props: any) => ({
     body: {
-      bg: mode('#f0e7db', '#202023')(props)
+      bg: mode('#fffbff', '#202023')(props)
     }
   })
 }
